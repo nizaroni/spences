c8436f891f882cf4a5c8d27f23c7a887a148397e
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2014-10-27 22:50:47 -0400

    Add main browser domready event initialization

--- browser/js/main.js
+++ browser/js/main.js
@@ -1,1 +1,21 @@
-console.log('Mainly nothing...');
+var domready, spences;
+
+domready = require('domready');
+
+function Spences () {}
+
+Spences.prototype.blastOff = function () {
+    var self = this;
+
+    domready(function initilizeView () {
+        var h1 = document.createElement('h1');
+        h1.textContent = 'Spences';
+        document.body.appendChild(h1);
+    });
+};
+
+spences = new Spences();
+module.exports = spences;
+window.spences = spences;
+
+spences.blastOff();
