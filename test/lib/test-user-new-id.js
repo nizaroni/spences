e9725c3f74626c053cbe4c393feed471e7062e8d
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2014-11-02 20:14:22 -0500

    Add `user-new-id` module
    Increment `state:user:ids` key and return new value.

---
+++ test/lib/test-user-new-id.js
@@ -0,0 +1,38 @@
+var series, test, userNewId, prefix, db;
+
+series = require('run-series');
+test = require('tape');
+
+userNewId = require('../../lib/user-new-id');
+prefix = require('../../lib/redis-key-prefix');
+db = require('../../lib/db')
+
+test('userNewId() test', function (t) {
+    function resetIds (callback) {
+        db.set(prefix() + 'state:user:ids', 0, callback);
+    }
+
+    function newId (callback) {
+        userNewId(callback)
+    }
+
+    function deliverKey (callback) {
+        db.get(prefix() + 'state:user:ids', callback);
+    }
+
+    var tasks = [ resetIds, newId, deliverKey, newId, deliverKey ];
+
+    series(tasks, function (err, results) {
+        if (err) {
+            throw err;
+        }
+
+        t.equal(results[1], 1);
+        t.equal(results[3], 2);
+        t.equal(+results[2], 1);
+        t.equal(+results[4], 2);
+
+        db.quit();
+        t.end();
+    });
+});
