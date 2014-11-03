1e7701b3f04bb25146f8a01a8c50a5aee4443944
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2014-11-03 13:57:26 -0500

    Use local storage token to stay logged in

--- browser/js/main.js
+++ browser/js/main.js
@@ -1,17 +1,32 @@
-var domready, navigate, MainView, router, spences;
+var parallel, domready, store, TokenModel, navigate, MainView, MeModel, router, me, spences;
 
+parallel = require('run-parallel');
 domready = require('domready');
+store = require('store');
 
+TokenModel = require('./models/token-model');
 navigate = require('./helpers/navigate');
 MainView = require('./views/main-view');
+MeModel = require('./models/me-model');
 router = require('./router');
+me = require('./helpers/me');
 
 function Spences () {}
 
 Spences.prototype.blastOff = function () {
-    var self = this;
+    var self;
 
-    domready(function initilizeView () {
+    self = this;
+
+    function user (callback) {
+        self.fetchUser(callback);
+    }
+
+    function dom (callback) {
+        domready(callback);
+    }
+
+    parallel([ user, dom ], function initilizeView () {
         var mainView;
 
         mainView = new MainView({ el: document.body });
@@ -19,6 +34,26 @@ Spences.prototype.blastOff = function () {
         self.view = mainView;
 
         router.history.start({ pushState: true, root: '/' });
+    });
+};
+
+Spences.prototype.fetchUser = function (callback) {
+    var token;
+
+    if (!store.get('token')) {
+        callback();
+        return;
+    }
+
+    token = new TokenModel();
+    token.fetch({
+        success: function saveMe (token, response) {
+            var newMe;
+
+            newMe = new MeModel(response);
+            me(newMe);
+            callback();
+        }
     });
 };
 
--- browser/js/models/token-model.js
+++ browser/js/models/token-model.js
@@ -1,6 +1,7 @@
-var AmpersandModel, TokenModel;
+var AmpersandModel, store, TokenModel;
 
 AmpersandModel = require('ampersand-model');
+store = require('store');
 
 TokenModel = AmpersandModel.extend({
     type: 'token',
@@ -14,7 +15,21 @@ TokenModel = AmpersandModel.extend({
         token: 'string'
     },
 
-    url: '/api/login'
+    url: '/api/login',
+
+    ajaxConfig: function setTokenHeader () {
+        var config;
+
+        config = {};
+
+        if (store.get('token')) {
+            config.headers = {
+                Authorization: 'Bearer ' + store.get('token')
+            };
+        }
+
+        return config;
+    }
 });
 
 module.exports = TokenModel;
