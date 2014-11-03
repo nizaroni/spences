8bbb3ef93794d1a215053a88133cfc75f1fde067
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2014-11-03 13:31:59 -0500

    Use global secret to sign JWTs

--- test/lib/test-jwt-sign.js
+++ test/lib/test-jwt-sign.js
@@ -1,4 +1,4 @@
-var hashy, auto, test, jwt, jwtSign;
+var hashy, auto, test, jwt, jwtSign, env;
 
 hashy = require('hashy');
 auto = require('run-auto');
@@ -7,37 +7,32 @@ jwt = require('jsonwebtoken');
 
 staggerResult = require('../stagger-result');
 jwtSign = require('../../lib/jwt-sign');
+env = require('../../lib/env');
 
 test('jwtSign() test', function jwtSignTest (t) {
-    var alfred, options, tasks;
+    var alfred, token, options, tasks;
 
     alfred = {
         id: 16,
         password: 'masterwayne'
     };
 
+    token = jwtSign(alfred);
     options = { issuer: 'spences-test' };
 
     tasks = {};
-    tasks.hashPassword = function hashPassword (callback) {
-        hashy.hash(alfred.password, callback);
+    tasks.verify = function verify (callback, results) {
+        jwt.verify(token, env.get('jwtSecret'), options, callback);
     };
-    tasks.signToken = ['hashPassword', function signToken (callback, results) {
-        alfred.passwordHash = results.hashPassword;
-        callback(null, jwtSign(alfred));
-    }];
-    tasks.verify = ['signToken', function verify (callback, results) {
-        jwt.verify(results.signToken, alfred.passwordHash, options, callback);
-    }];
-    tasks.badSecret = ['signToken', function badSecret (callback, results) {
-        jwt.verify(results.signToken, 'dfsfsdfsdfsdf', options, staggerResult(callback));
-    }];
-    tasks.badIssuer = ['signToken', function badIssuer (callback, results) {
+    tasks.badSecret = function badSecret (callback, results) {
+        jwt.verify(token, 'dfsfsdfsdfsdf', options, staggerResult(callback));
+    };
+    tasks.badIssuer = function badIssuer (callback, results) {
         var badOptions;
 
         badOptions = { issuer: 'spencesdjfsdfksdkfj' };
-        jwt.verify(results.signToken, alfred.passwordHash, badOptions, staggerResult(callback));
-    }];
+        jwt.verify(token, env.get('jwtSecret'), badOptions, staggerResult(callback));
+    };
 
     auto(tasks, function performTests (err, results) {
         t.error(err);
