d8d88e3933ab31996ab6042562dd5cebd26e7d8f
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2014-11-02 20:22:17 -0500

    Add prefix to user key

--- test/lib/test-user-key.js
+++ test/lib/test-user-key.js
@@ -1,14 +1,21 @@
-var test, userKey;
+var test, userKey, env;
 
 test = require('tape');
 
 userKey = require('../../lib/user-key');
+env = require('../../lib/env');
 
 test('userKey() test', function (t) {
     var key;
 
     key = userKey('alfred@batmail.com');
-    t.equal(key, 'user:alfred@batmail.com');
+    t.equal(key, 'test:user:alfred@batmail.com');
 
-    t.end();
+    env.temp({ redis_key_prefix: '' }, function () {
+        key = userKey('alfred@batmail.com');
+        t.equal(key, 'user:alfred@batmail.com');
+
+        t.end();
+    });
+
 });
