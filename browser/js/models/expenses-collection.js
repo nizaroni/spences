956685a26af29d818be985aa8dadabd39490c743
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2014-11-03 19:24:05 -0500

    Add token to expenses collection requests

--- browser/js/models/expenses-collection.js
+++ browser/js/models/expenses-collection.js
@@ -1,13 +1,28 @@
-var AmpersandRestCollection, ExpenseModel, ExpensesCollection;
+var AmpersandRestCollection, store, ExpenseModel, ExpensesCollection;
 
 AmpersandRestCollection = require('ampersand-rest-collection');
+store = require('store');
 
 ExpenseModel = require('./expense-model');
 
 ExpensesCollection = AmpersandRestCollection.extend({
     model: ExpenseModel,
 
-    url: '/api/expenses'
+    url: '/api/expenses',
+
+    ajaxConfig: function setTokenHeader () {
+        var config;
+
+        config = {};
+
+        if (store.get('token')) {
+            config.headers = {
+                Authorization: 'Bearer ' + store.get('token')
+            };
+        }
+
+        return config;
+    }
 });
 
 module.exports = new ExpensesCollection();
