e8f1d7c15c0701f4acc38f110df173fc378468c4
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2014-11-03 20:22:29 -0500

    Add search to expenses page

--- browser/js/pages/expenses-page.js
+++ browser/js/pages/expenses-page.js
@@ -12,12 +12,52 @@ ExpensesPage = AmpersandView.extend({
 
     authOnly: true,
 
+    events: {
+        'keyup [data-hook~=expense-search]': 'filterExpenses'
+    },
+
     render: function () {
         this.renderWithTemplate();
         this.renderCollection(expensesCollection, ExpenseItemView, this.queryByHook('expenses-list'));
         if (!expensesCollection.length) {
             expensesCollection.fetch();
         }
+    },
+
+    filterExpenses: function (event) {
+        var searchInput, filtered;
+
+        if (this.isFiltering) {
+            return;
+        }
+
+        if (!this.allExpenses) {
+            this.allExpenses = expensesCollection.models.slice();
+        }
+
+        searchInput = event.target.value.toLowerCase();
+
+        if (!searchInput) {
+            expensesCollection.set(this.allExpenses);
+            return;
+        }
+
+        this.isFiltering = true;
+        filtered = this.allExpenses.filter(function searchExpenses (expense) {
+            var stringAmount;
+
+            stringAmount = '' + expense.prettyAmount;
+            return stringAmount.indexOf(searchInput) > -1
+                || expense.date.indexOf(searchInput) > -1
+                || expense.time.indexOf(searchInput) > -1
+                || expense.description.toLowerCase().indexOf(searchInput) > -1
+                || expense.comments.toLowerCase().indexOf(searchInput) > -1
+            ;
+        });
+
+        expensesCollection.set(filtered);
+
+        this.isFiltering = false;
     }
 });
 
