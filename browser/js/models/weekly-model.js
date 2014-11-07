bd4f4525a1b2696a9cb89b3d88eb5d924af1bc59
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2014-11-07 15:10:39 -0500

    Add weekly model

---
+++ browser/js/models/weekly-model.js
@@ -0,0 +1,46 @@
+var AmpersandState, WeeklyModel;
+
+AmpersandState = require('ampersand-state');
+
+WeeklyModel = AmpersandState.extend({
+    type: 'weekly',
+
+    props: {
+        name: [ 'string', true ],
+        amount: [ 'number', true, 0 ],
+        total: [ 'number', true, 0 ]
+    },
+
+    derived: {
+        dailyAverage: {
+            deps: [ 'total' ],
+            fn: function () {
+                if (!this.amount || !this.total) {
+                    return 0;
+                }
+                return Math.round(this.amount / this.total);
+            }
+        },
+
+        prettyDailyAverage: {
+            deps: [ 'dailyAverage' ],
+            fn: function () {
+                return this.dailyAverage / 100;
+            }
+        },
+
+        prettyAmount: {
+            deps: [ 'amount' ],
+            fn: function () {
+                return this.amount / 100;
+            }
+        }
+    },
+
+    add: function (addedAmount) {
+        this.amount += addedAmount;
+        this.total += 1;
+    }
+});
+
+module.exports = WeeklyModel;
