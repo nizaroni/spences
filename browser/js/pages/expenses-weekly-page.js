3c497054f34e7847a649d99fadd5b5055860ca4b
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2014-11-07 15:43:51 -0500

    Render weekly expenses in weeklies page

--- browser/js/pages/expenses-weekly-page.js
+++ browser/js/pages/expenses-weekly-page.js
@@ -1,9 +1,11 @@
-var AmpersandView, expensesCollection, expensesWeekly, ExpensesWeeklyPage;
+var AmpersandView, expensesCollection, weeklyCollection, expensesWeekly, WeeklyItemView, ExpensesWeeklyPage;
 
 AmpersandView = require('ampersand-view');
 
 expensesCollection = require('../models/expenses-collection');
+weeklyCollection = require('../models/weekly-collection');
 expensesWeekly = require('../../templates/pages/expenses-weekly.dom');
+WeeklyItemView = require('../views/weekly-item-view');
 
 ExpensesWeeklyPage = AmpersandView.extend({
     pageTitle: 'Weekly Breakdown | Spences',
@@ -25,6 +27,14 @@ ExpensesWeeklyPage = AmpersandView.extend({
     },
 
     expenses: expensesCollection,
+
+    render: function () {
+        this.renderWithTemplate();
+        this.renderCollection(weeklyCollection, WeeklyItemView, this.queryByHook('weekly-list'));
+        if (!expensesCollection.filterQuery && !expensesCollection.length) {
+            expensesCollection.fetch();
+        }
+    },
 
     filterExpenses: function (event) {
         expensesCollection.filterExpenses(event.target.value);
