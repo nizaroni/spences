edbd7e899778fd3733128390dafe5ef4c05c27be
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2014-11-07 13:26:50 -0500

    Add expenses search to weekly breakdown

--- browser/js/pages/expenses-weekly-page.js
+++ browser/js/pages/expenses-weekly-page.js
@@ -1,13 +1,34 @@
-var AmpersandView, expensesWeekly, ExpensesWeeklyPage;
+var AmpersandView, expensesCollection, expensesWeekly, ExpensesWeeklyPage;
 
 AmpersandView = require('ampersand-view');
 
+expensesCollection = require('../models/expenses-collection');
 expensesWeekly = require('../../templates/pages/expenses-weekly.dom');
 
 ExpensesWeeklyPage = AmpersandView.extend({
     pageTitle: 'Weekly Breakdown | Spences',
 
-    template: expensesWeekly
+    template: expensesWeekly,
+
+    authOnly: true,
+
+    events: {
+        'keyup [data-hook~=expense-search]': 'filterExpenses'
+    },
+
+    bindings: {
+        'expenses.filterQuery': {
+            type: 'attribute',
+            name: 'value',
+            hook: 'expense-search'
+        }
+    },
+
+    expenses: expensesCollection,
+
+    filterExpenses: function (event) {
+        expensesCollection.filterExpenses(event.target.value);
+    }
 });
 
 module.exports = ExpensesWeeklyPage;
